bdf5a13694b85dd24e2453a07d7011fa2d0c0ce9
ivayloyovkov <ivayloyovkovofficial@gmail.com>
2020-02-02 15:15:06 +0200

    Updates

---
+++ precache-manifest.bc72133049e5d00bacfe2272782e802e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cdcfa20d750caf1478ce11e702100f2",
+    "url": "/RoboFriends/index.html"
+  },
+  {
+    "revision": "efb5c163f9e0875aadb5",
+    "url": "/RoboFriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "2ed26e2a24acd92fdc69",
+    "url": "/RoboFriends/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "efb5c163f9e0875aadb5",
+    "url": "/RoboFriends/static/js/2.4782cfe2.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/RoboFriends/static/js/2.4782cfe2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ed26e2a24acd92fdc69",
+    "url": "/RoboFriends/static/js/main.11dddccf.chunk.js"
+  },
+  {
+    "revision": "db7d681b12c63738cbc7",
+    "url": "/RoboFriends/static/js/runtime-main.11cb84f8.js"
+  }
+]);
